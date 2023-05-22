7280b210c3ed76ce3f5756d57bd508a3a516d74b
Matheus Reis <reis.mmatheus@gmail.com>
2023-05-22 08:55:12 -0300

    Adding Structure

---
+++ ScheduleExamsApi/ScheduleExample.API/Controllers/ClientController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScheduleExams.Application.Interface;
+
+namespace ScheduleExams.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : Controller
+    {
+        private readonly IClientBusiness _authBusiness;
+        public ClientController(IClientBusiness authBusiness)
+        {
+            _authBusiness = authBusiness;
+        }
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddAsync()
+        {
+            return Ok();
+        }
+        [HttpPost("Update")]
+        public async Task<IActionResult> UpdateAsync()
+        {
+            return Ok();
+        }
+        [HttpPost("Delete")]
+        public async Task<IActionResult> DeleteAsync()
+        {
+            return Ok();
+        }
+        [HttpGet("{cpf}")]
+        public async Task<IActionResult> GetByCpfAsync(string cpf)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ScheduleExamsApi/ScheduleExams.Application/Business/ClientBusiness.cs
@@ -0,0 +1,13 @@
+﻿using ScheduleExams.Application.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleExams.Application.Business
+{
+    public class ClientBusiness : IClientBusiness
+    {
+    }
+}
